ccabdd2691a742139834ac41f83c3075e7fcce5e
Tom <>
2023-09-02 18:22:23 +0200

    Small changes

--- autoLightmode.js
+++ autoLightmode.js
@@ -2,7 +2,7 @@
 
 // NAME: AutoLightmode
 // AUTHOR: Codedotexe
-// DESCRIPTION: Query a local server to detect if the system is in dark mode and add a class label for the custom theme
+// DESCRIPTION: Add a button to switch between dark and light colorscheme, requires additional setup
 
 /// <reference path="../globals.d.ts" />
 
@@ -20,7 +20,7 @@
 		}
 	}
 
-	new Spicetify.Topbar.Button("Switch theme", `<svg version="1.1" id="mdi-theme-light-dark" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg"><defs id="defs7"/><path d="M7.5,2C5.71,3.15 4.5,5.18 4.5,7.5C4.5,9.82 5.71,11.85 7.53,13C4.46,13 2,10.54 2,7.5A5.5,5.5 0 0,1 7.5,2M19.07,3.5L20.5,4.93L4.93,20.5L3.5,19.07L19.07,3.5M12.89,5.93L11.41,5L9.97,6L10.39,4.3L9,3.24L10.75,3.12L11.33,1.47L12,3.1L13.73,3.13L12.38,4.26L12.89,5.93M9.59,9.54L8.43,8.81L7.31,9.59L7.65,8.27L6.56,7.44L7.92,7.35L8.37,6.06L8.88,7.33L10.24,7.36L9.19,8.23L9.59,9.54M19,13.5A5.5,5.5 0 0,1 13.5,19C12.28,19 11.15,18.6 10.24,17.93L17.93,10.24C18.6,11.15 19,12.28 19,13.5M14.6,20.08L17.37,18.93L17.13,22.28L14.6,20.08M18.93,17.38L20.08,14.61L22.28,17.15L18.93,17.38M20.08,12.42L18.94,9.64L22.28,9.88L20.08,12.42M9.63,18.93L12.4,20.08L9.87,22.27L9.63,18.93Z" id="path2" style="fill:#fff;fill-opacity:1"/></svg>`, toggleLightmode);
+	new Spicetify.Topbar.Button('Switch theme', `<svg width="20" height="20" version="1.1" viewBox="0 0 20 20" xml:space="preserve" xmlns="http://www.w3.org/2000/svg"><path d="m10 14a4 4 0 0 0 0-8z" fill="#fff"/><path d="m10 0c-5.523 0-10 4.477-10 10s4.477 10 10 10 10-4.477 10-10-4.477-10-10-10zm0 2v4a4 4 0 1 0 0 8v4a8 8 0 1 0 0-16z" clip-rule="evenodd" fill="#fff" fill-rule="evenodd"/></svg>`, toggleLightmode);
 	
 	Spicetify.Keyboard.registerShortcut({
 		key: Spicetify.Keyboard.KEYS["N"], 
