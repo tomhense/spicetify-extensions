ecde4c8b4ac23cfeb0194cd71f49e1612ce031c9
Tom <>
2024-07-11 04:43:59 +0200

    Removed sidebarToggle script since that functionality is now in spotify buildin. Added new feature to autoLightmode script that polls a server for system theme info, I will later also publish a simple nodejs server that serves this information based on linux dbus information.

--- autoLightmode.js
+++ autoLightmode.js
@@ -7,12 +7,16 @@
 /// <reference path="../globals.d.ts" />
 
 (function autoLightmode() {
+	const checkDarkModeServer = "http://127.0.0.1:8160";
+	const checkDarkModeServerEnabled = true;
+
 	if (!Spicetify.Keyboard) {
 		setTimeout(autoLightmode, 1000);
 		return;
 	}
 
 	function toggleLightmode() {
+		document.body.classList.add("lightmode-manual");
 		if (document.body.classList.contains("lightmode")) {
 			document.body.classList.remove("lightmode");
 		} else {
@@ -20,12 +24,41 @@
 		}
 	}
 
-	new Spicetify.Topbar.Button('Switch theme', `<svg width="20" height="20" version="1.1" viewBox="0 0 20 20" xml:space="preserve" xmlns="http://www.w3.org/2000/svg"><path d="m10 14a4 4 0 0 0 0-8z" fill="#fff"/><path d="m10 0c-5.523 0-10 4.477-10 10s4.477 10 10 10 10-4.477 10-10-4.477-10-10-10zm0 2v4a4 4 0 1 0 0 8v4a8 8 0 1 0 0-16z" clip-rule="evenodd" fill="#fff" fill-rule="evenodd"/></svg>`, toggleLightmode);
-	
-	Spicetify.Keyboard.registerShortcut({
-		key: Spicetify.Keyboard.KEYS["N"], 
-		ctrl: true, 
-		shift: false,
-		alt: false
-	}, toggleLightmode);
+	async function checkServer() {
+		if (!checkDarkModeServerEnabled) return;
+		const res = await fetch(checkDarkModeServer, { method: "GET", headers: { "Content-Type": "application/json" } })
+			.then((res) => res.json())
+			.catch((err) => console.error(err));
+
+		if (document.body.classList.contains("lightmode-manual")) return; // Don't override manual theme
+
+		if (res.theme === "light") {
+			document.body.classList.add("lightmode");
+		} else if (res.theme === "dark") {
+			document.body.classList.remove("lightmode");
+		}
+	}
+
+	const iconSVG = `<svg width="20" height="20" version="1.1" viewBox="0 0 20 20" xml:space="preserve" xmlns="http://www.w3.org/2000/svg">
+		<circle cx="10" cy="10" r="10" fill="#000"/>
+		<path d="m10 14a4 4 0 0 0 0-8z" fill="#fff"/>
+		<path d="m10 0c-5.523 0-10 4.477-10 10s4.477 10 10 10 10-4.477 10-10-4.477-10-10-10zm0 2v4a4 4 0 1 0 0 8v4a8 8 0 1 0 0-16z" clip-rule="evenodd" fill="#fff" fill-rule="evenodd"/>
+	</svg>`;
+
+	new Spicetify.Topbar.Button("Switch theme", iconSVG, toggleLightmode);
+
+	Spicetify.Keyboard.registerShortcut(
+		{
+			key: Spicetify.Keyboard.KEYS["N"],
+			ctrl: true,
+			shift: false,
+			alt: false,
+		},
+		toggleLightmode
+	);
+
+	if (checkDarkModeServerEnabled) {
+		setInterval(checkServer, 1000 * 30);
+		checkServer();
+	}
 })();
